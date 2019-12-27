fb3bbef313c15e3bb8775ce4b0b7c9effbdd1d0f
Sky1Wu <wtx919463944@gmail.com>
2019-12-27 23:05:52 +0800

    细节优化

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Campus_Network_Login
 
 校园网自动登录脚本
+
+如需删除已保存的账号密码，需显示系统隐藏文件，删除脚本同目录下的 `campus_network_data` 文件。
--- wifi.py
+++ wifi.py
@@ -1,16 +1,25 @@
 import requests
 import os
 import pickle
+import json
+import getpass
+from time import sleep
 import win32api
 import win32con
 
 file_name = 'campus_network_data'
 
+
+def exit_program():
+    os.system("pause")
+    exit()
+
+
 print('校园网一键认证')
 if not os.path.exists(file_name):
     print('首次运行请输入登录信息')
     username = input('账号：')
-    password = input('密码：')
+    password = getpass.getpass('密码（输入不显示）：')
 
     file = open(file_name, 'wb')
     pickle.dump([username, password], file)
@@ -39,22 +48,30 @@ while True:
     }
 
     # 登录
-    login_response = eval(requests.post(
-        login_url, headers=login_header, data=data).content.decode('utf-8'))
+    try:
+        login_response = requests.post(
+            login_url, headers=login_header, data=data)
+        login_response = json.loads(login_response.content.decode('utf-8'))
 
-    if 'content' in login_response.keys():
-        print('帐号或密码错误')
-        username = input('帐号：')
-        password = input('密码：')
+        try:
+            access_token = login_response['access_token']
+            break
+        except KeyError:
+            print('帐号或密码错误')
+            username = input('帐号：')
+            password = getpass.getpass('密码（输入不显示）：')
 
-        win32api.SetFileAttributes(file_name, win32con.FILE_ATTRIBUTE_NORMAL)
-        file = open(file_name, 'wb')
-        pickle.dump([username, password], file)
-        file.close()
-        win32api.SetFileAttributes(file_name, win32con.FILE_ATTRIBUTE_HIDDEN)
+            win32api.SetFileAttributes(
+                file_name, win32con.FILE_ATTRIBUTE_NORMAL)
+            file = open(file_name, 'wb')
+            pickle.dump([username, password], file)
+            file.close()
+            win32api.SetFileAttributes(
+                file_name, win32con.FILE_ATTRIBUTE_HIDDEN)
+    except:
+        print('无法连接至校园网')
+        exit_program()
 
-    else:
-        break
 
 access_token = login_response['access_token']
 status_url = 'http://172.16.251.172:8081/portal/wifi/status'
@@ -64,28 +81,37 @@ auth_header = {
     'Authorization': 'bearer '+access_token
 }
 
-status_response = eval(requests.post(
-    status_url, headers=auth_header).content.decode('utf-8'))
+try:
+    status_response = requests.post(status_url, headers=auth_header)
+    status_response = json.loads(status_response.content.decode('utf-8'))
+    code = status_response['code']
+    message = status_response['message']
 
-code = status_response['code']
-message = status_response['message']
+except:
+    print('状态获取失败')
+    exit_program()
 
 online_code = '12001'
 
 if code != online_code:
     url = 'http://172.16.251.172:8081/portal/wifi/login'
-    auth_response = eval(requests.post(
-        url, headers=auth_header).content.decode('utf-8'))
+    try:
+        auth_response = requests.post(url, headers=auth_header)
+        auth_response = json.loads(auth_response.content.decode('utf-8'))
+        code = auth_response['code']
 
-    # 重新检查认证状态
-    status_response = eval(requests.post(
-        status_url, headers=auth_header).content.decode('utf-8'))
-    code = status_response['code']
-    message = status_response['message']
-
-    print('认证状态：'+message, 'Code: '+code)
+        if(code == online_code):
+            print('认证成功')
+            sleep(1)
+        else:
+            print('认证失败')
+            print(auth_response)
+            exit_program()
+    except:
+        print('认证失败')
+        print(auth_response)
+        exit_program()
 
 else:
     print("当前在线")
-
-os.system("pause")
+    sleep(1)
